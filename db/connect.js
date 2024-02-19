21b48641dee5dfd61a987734e30f3d75504ab30a
Tina Huynh Le <huynhletina@gmail.com>
2024-02-19 14:09:33 +0100

    move if statement inside function in connect.js, so app won't crash

--- db/connect.js
+++ db/connect.js
@@ -4,12 +4,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log("MONGODB_URI is: ", MONGODB_URI);
 
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -22,6 +16,12 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
