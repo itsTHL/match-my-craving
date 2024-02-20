b5f380d16fcde8c7e85a477e4d35830b03dafc12
Tina Huynh Le <huynhletina@gmail.com>
2024-02-20 13:24:33 +0100

    add basic styling to body and main

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <div>
+        <div className={`${styles.index__container}`}>
           <Image
             src="/salad.jpg"
             alt="photo of a salad"
