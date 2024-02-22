61f6e2ab0d2ccbee3123f327299c895fa2040208
Tina Huynh Le <huynhletina@gmail.com>
2024-02-22 14:11:07 +0100

    write counter component, render counter in start session oage

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
--- src/pages/[id]/startsession.js
+++ src/pages/[id]/startsession.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import FoodPref from "@/components/FoodPref";
+import Counter from "@/components/Counter";
 
 export default function startsession() {
   return (
     <>
       <Image src="/salad.jpg" alt="photo of a salad" width="150" height="150" />
-      {/* COUNTER components */}
+      <Counter />
       <FoodPref />
       <button>Start the Session!</button>
     </>
