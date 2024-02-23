7ea4fd0627faab2aa3303266d5f0a0efc74acd4f
Tina Huynh Le <huynhletina@gmail.com>
2024-02-23 16:21:06 +0100

    write MessageField component

---
+++ src/components/MessageField.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function MessageField({ roomId }) {
+  const [input, SetInput] = useState();
+
+  async function sendMessage(text) {
+    try {
+      await fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text, roomId }),
+      });
+    } catch (error) {
+      // Handle error
+      console.error("Error sending message:", error);
+    }
+  }
+
+  return (
+    <div>
+      type a new message:
+      <input onChange={({ target }) => setInput(target.value)} type="text" />
+      <button onClick={() => sendMessage(input || "")}>send</button>
+    </div>
+  );
+}
