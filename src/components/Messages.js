d336b266a95f2f615794b5110f3682643834888f
Tina Huynh Le <huynhletina@gmail.com>
2024-02-23 16:13:42 +0100

    write Messages component

---
+++ src/components/Messages.js
@@ -0,0 +1,29 @@
+import { pusherClient } from "@/lib/pusher";
+import { useEffect, useState } from "react";
+
+export default function Messages({ initialMessages, roomId }) {
+  const [incomingMessages, setIncomingMessages] = useState([]);
+
+  useEffect(() => {
+    pusherClient.subscribe(roomId);
+
+    pusherClient.bind("incoming-message", (text) => {
+      setIncomingMessages((prev) => [...prev, text]);
+    });
+
+    return () => {
+      pusherClient.unsubscribe(roomId);
+    };
+  }, []);
+
+  return (
+    <div>
+      {initialMessages.map((message) => (
+        <p key={message.id}>{message.text}</p>
+      ))}
+      {incomingMessages.map((text, i) => (
+        <p key={i}>{text}</p>
+      ))}
+    </div>
+  );
+}
