ccded1f837e19b5a0c1812f6ab2d6bbce3c11e91
Tina Huynh Le <huynhletina@gmail.com>
2024-02-24 23:02:50 +0100

    add back button to details page

--- src/pages/myrecipes/[id].js
+++ src/pages/myrecipes/[id].js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Login from "@/components/Login";
+import Link from "next/link";
 
 export default function RecipeDetailsPage() {
   const { data: session } = useSession();
@@ -27,6 +28,9 @@ export default function RecipeDetailsPage() {
           <Image src="/salad.jpg" alt="" width="150" height="150" />
           <h2>{recipe.title}</h2>
           <p>{recipe.comment ? recipe.comment : null}</p>
+          <button type="button">
+            <Link href="/myrecipes">Back to all recipes</Link>
+          </button>
         </>
       )}
     </>
