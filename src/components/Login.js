550a772a61302c123bd3749e856eb3ddc16456af
Tina Huynh Le <huynhletina@gmail.com>
2024-02-26 14:51:41 +0100

    add logo to sign in page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import { signIn, signOut } from "next-auth/react";
+import styles from "./Login.module.css";
 
 export default function Login() {
   return (
     <>
-      <h2>You are not signed in.</h2>
-      <Image src="/salad.jpg" alt="photo of a salad" width="150" height="150" />
-      <h2>Sign in with Google</h2>
-      <button onClick={() => signIn("google")}>Sign in</button>
+      <Image
+        src="/MMC_Logo_Black.png"
+        alt="MMC Logo"
+        width="300"
+        height="200"
+        className={`${styles.logo}`}
+      />
+      <h2 className={`${styles.heading}`}>You are not signed in.</h2>
+      <button onClick={() => signIn("google")}>Sign in now</button>
     </>
   );
 }
