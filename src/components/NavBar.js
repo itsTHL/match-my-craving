edd7a6c258abd4d308978e287077f327b18c0f8c
Tina Huynh Le <huynhletina@gmail.com>
2024-02-26 19:10:08 +0100

    finish nav bar move logo into nav bar add back button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,11 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   return (
     <nav className={`${styles.nav}`}>
-      <button>Home</button>
       <Image
         src="/MMC_Logo_Black.png"
         alt="MMC Logo"
-        width="150"
-        height="100"
+        width="200"
+        height="150"
         className={`${styles.nav_logo}`}
       />
       <button>Back</button>
