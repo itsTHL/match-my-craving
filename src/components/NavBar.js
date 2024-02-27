d8d81df41b796ba08e16580ae66eb8d8b93c38c4
Tina Huynh Le <huynhletina@gmail.com>
2024-02-27 19:29:23 +0100

    add link to homepage to logo in nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./NavBar.module.css";
 import BackButton from "./BackButton";
 
 export default function NavBar() {
   return (
     <nav className={`${styles.nav}`}>
-      <Image
-        src="/MMC_Logo_Black.png"
-        alt="MMC Logo"
-        width="200"
-        height="150"
-        className={`${styles.nav_logo}`}
-      />
+      <Link href="/">
+        <Image
+          src="/MMC_Logo_Black.png"
+          alt="MMC Logo"
+          width="200"
+          height="150"
+          className={`${styles.nav_logo}`}
+        />
+      </Link>
       <BackButton />
     </nav>
   );
