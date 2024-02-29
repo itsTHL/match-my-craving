faad4c98a7a7f8357857d075e7124245179a3742
Tina Huynh Le <huynhletina@gmail.com>
2024-02-29 18:34:41 +0100

    refactor nav bar to header and adjust logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Provider from "@/components/Provider";
 import Layout from "@/components/Layout";
 import { SWRConfig } from "swr";
+import Header from "@/components/Header";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
