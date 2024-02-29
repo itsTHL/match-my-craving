ddda74ee85109b4daa2b308c6587214da92c87c2
Tina Huynh Le <huynhletina@gmail.com>
2024-02-29 14:05:41 +0100

    change favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Head>
         <title>Match my Craving</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       {!session ? (
